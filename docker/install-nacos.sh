73708f22a83752d8963ce1ab4b15f732868fa454
richard <richard123@ex.cc>
2020-06-14 01:47:35 +0800

    nacos mysql connection

--- docker/install-nacos.sh
+++ docker/install-nacos.sh
@@ -4,6 +4,10 @@ unzip nacos-server-1.3.0.zip
 rm -fr nacos/conf/application.properties
 
 cp application.properties nacos/conf/
+
+#主要是为了解决数据连接问题
+mv application.properties nacos/conf/application.properties application.properties nacos/conf/bootstrap.properties
+
 cd nacos/bin
 
 
---
+++ docker/readme.errors.md
@@ -0,0 +1,4 @@
+# 注意事项
+## 在linux上,如果用nacos.sh安装的nocas可能会连接不上数据库
+解决办法:
+把application.properties
