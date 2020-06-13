cd3a1bb98429094bb6ed593a34d51642f292a852
richard <richard123@ex.cc>
2020-06-13 14:08:01 +0800

    增加docker配置

---
+++ docker/installoncentos.sh
@@ -0,0 +1,30 @@
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+
+
+
+
+ sudo yum install -y yum-utils
+
+ sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+
+sudo yum install docker-ce docker-ce-cli containerd.io -y
+
+
+sudo systemctl start docker
+
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+
+sudo chmod +x /usr/local/bin/docker-compose
